a7a06dfbb55ffd7c46d3f71ab289168b60760bae
mallorax <patryk733773@gmail.com>
2019-09-12 17:29:02 +0200

    step 2 completed

--- app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
+++ app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
@@ -27,8 +27,7 @@ class MainActivity : AppCompatActivity() {
      *
      * Delegate this to Navigation.
      */
-    override fun onSupportNavigateUp()
-            = navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
+    override fun onSupportNavigateUp() = navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
 
     /**
      * Setup Navigation for this Activity
@@ -48,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
             val toolBar = supportActionBar ?: return@addOnDestinationChangedListener
-            when(destination.id) {
+            when (destination.id) {
                 R.id.home -> {
                     toolBar.setDisplayShowTitleEnabled(false)
                     binding.heroImage.visibility = View.VISIBLE
